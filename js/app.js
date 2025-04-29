0b2de53447ea897265a369058e1064f9a24a2b3f
carloscMe <carloscarrillo.sprout@gmail.com>
2025-04-29 18:11:23 -0500

    api

--- js/app.js
+++ js/app.js
@@ -3,6 +3,19 @@ const lista = document.querySelector("ul");
 const imagen = document.querySelector(".img");
 let visible = false;
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('item--caja2');   // con esta api le aplico el view aunque necesito probarla mas
+      }
+    });
+  });
+  
+  document.querySelectorAll('.item--caja').forEach(box => {
+    observer.observe(box);
+  });
+  
+
 const menu = {
 
     abrir : "./assets/menu.png",
